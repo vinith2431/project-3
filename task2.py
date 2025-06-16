9900a9a94fc71edd1ba1faec67e8c77181acd04f
vinith2431 <vinith2431@gmail.com>
2025-06-16 15:28:44 +0530

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,7 @@
+import math
+
+num = float(input("Enter a number: "))
+
+print("Square root:", math.sqrt(num))
+print("Natural logarithm:", math.log(num))
+print("Sine (in radians):", math.sin(num))
